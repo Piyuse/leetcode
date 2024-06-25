83d5546a3e298bc03e5cf430c9b174450ff6a60e
Priya <priyapiyuse@gmail.com>
2024-06-25 13:11:49 +0530

    rotation by 90

---
+++ ror_90.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+void rotate_90(int A[20][20], int m, int n)
+{
+    int i, j, temp, k;
+    // transpose of matrix
+    for (i = 0; i < m; i++)
+    {
+        for (j = i; j < n; j++)
+        {
+            temp = A[i][j];
+            A[i][j] = A[j][i];
+            A[j][i] = temp;
+        }
+    }
+    printf("matrix after having 90 degree rotation\n");
+    for (j = 0; j < n; j++)
+    {
+        for (i = 0, temp; i < n / 2; i++)
+        {
+            temp = A[i][j];
+            A[i][j] = A[n - 1 - i][j];
+            A[n - 1 - i][j] = temp;
+        }
+    }
+}
+
+void print_matrix(int A[20][20], int m, int n)
+{
+    int i, j;
+
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int A[20][20], m, n, i, j;
+    printf("enter the size of matrix\n");
+    scanf("%d", &m);
+    scanf("%d", &n);
+    printf("enter the matrix\n");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("enter the elementA[%i][%i]", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+    printf("displaying matrix \n");
+    print_matrix(A, m, n);
+    rotate_90(A, m, n);
+    printf("matrix after 90 degree rotation is\n");
+    print_matrix(A, m, n);
+}
